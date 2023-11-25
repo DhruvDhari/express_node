b55ea4c9f6ae308906632fc6e5b69984ad0dab3d
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-11-25 23:41:15 +0530

    learnt about web sockets

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log(__dirname);
-console.log(__filename);
+const http=require('http')
+
+const server=http.createServer((req,res)=>{
+    console.log('request event');
+    res.end("hello world");
+})
+
+server.listen(5000,()=>{
+    console.log('sunn rha ha hoon on 5000..');
+})
